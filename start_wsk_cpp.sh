592eb97008288bb603371f72517dd4bccb96910a
delphieritas <34205896+delphieritas@users.noreply.github.com>
2022-03-21 16:04:16 +0800

    Update start_wsk_cpp.sh

--- start_wsk_cpp.sh
+++ start_wsk_cpp.sh
@@ -225,13 +225,13 @@ echo '
 import example
 if __name__ == "__main__":
     example.multiply(2,4)' > hello.py
-echo 'def __bootstrap__():
+echo "def __bootstrap__():
    global __bootstrap__, __loader__, __file__
    import sys, pkg_resources, imp
-   __file__ = pkg_resources.resource_filename(__name__,\'example.so\')
+   __file__ = pkg_resources.resource_filename(__name__,'example.so')
    __loader__ = None; del __bootstrap__, __loader__
    imp.load_dynamic(__name__,__file__)
-__bootstrap__()' > example.py
+__bootstrap__()" > example.py
 }
 
 set_cmakefile(){
